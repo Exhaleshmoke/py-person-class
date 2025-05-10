8ed8cb463bb1f45acb3209c48ebd08edea8091e8
Exhaleshmoke <voloshynserhii2005@gmail.com>
2025-05-10 19:44:57 +0100

    1.5

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data: list[dict]) -> list[Person]:
+    # Шаг 1: создать всех людей
+    persons = []
+    for data in people_data:
+        person = Person(name=data["name"], age=data["age"])
+        persons.append(person)
+
+    # Шаг 2: установить связи wife/husband
+    for data in people_data:
+        person = Person.people[data["name"]]
+        if "wife" in data and data["wife"] is not None:
+            person.wife = Person.people[data["wife"]]
+        if "husband" in data and data["husband"] is not None:
+            person.husband = Person.people[data["husband"]]
+
+    return persons
